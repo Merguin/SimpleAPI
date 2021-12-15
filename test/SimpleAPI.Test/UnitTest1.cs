a9627f6f06d21288ebdc435388ca35d054179427
sebastian <sebastian@elikosoft.com.mk>
2021-12-15 10:46:15 +0100

    2nd Commit with old .net

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -2,17 +2,20 @@ using System;
 using Xunit;
 using SimpleAPI.Controllers;
 
+
 namespace SimpleAPI.Test
 {
     public class UnitTest1
     {
-        WeatherForecastController controller = new WeatherForecastController();
+        ValuesController controller = new ValuesController();
         [Fact]
-        public void GetReturnsForecast()
+        public void GetReturnsMyName()
         {
             var returnValue = controller.Get(1);
-            Assert.Equal("Freezing",returnValue.Value);
+            Assert.Equal("Les Jackson", returnValue.Value);
         }
+
+
 
         [Fact]
         public void Test1()
