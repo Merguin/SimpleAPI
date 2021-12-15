950ce79683da441d47b1403958538f2130f0e74c
sebastian <sebastian@elikosoft.com.mk>
2021-12-15 10:21:04 +0100

    Fist Ever commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+        [Fact]
+        public void GetReturnsForecast()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Freezing",returnValue.Value);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
